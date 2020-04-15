9a586824c4d88163717cb413c3308c0fed39b007
brunoCreator <brunoCreator@users.noreply.github.com>
2020-04-15 14:23:07 -0300

    s

--- js/main.js
+++ js/main.js
@@ -1,20 +1,80 @@
-var wordlist = document.getElementById("text").innerHTML.split(' ');
-var i = 0;
+var categ = JSON.parse(jsonCateg);
+var posts = JSON.parse(jsonPosts);
 
-var n = document.getElementById("n").value;
+var menu = document.getElementById("posts");
+var folder = document.getElementById("folder");
 
-n = 60000/n;
 
-if(i < wordlist.length){
-  setInterval(words, n);
-}else{
-  document.getElementById("w").innerHTML = "acabo caralho";
+var quotes = JSON.parse(quote);
+
+
+
+document.getElementById("quot").innerHTML = quotes[Math.floor(Math.random() * quotes.length)][0];
+
+for (var key in posts) {
+    if (!posts.hasOwnProperty(key)) continue;
+
+    //var lista = document.createElement('li');
+
+    var link = document.createElement('div');
+    link.setAttribute("class", "link");
+    var linkA = document.createElement('a');
+    linkA.setAttribute("href", "#" + key);
+
+    linkH = document.createElement('h2');
+
+    linkH.appendChild(document.createTextNode(posts[key][0]));
+
+    var linkP = document.createElement('p');
+    linkP.appendChild(document.createTextNode(posts[key][4]));
+
+
+    var linkT = document.createElement('small');
+    //date
+    var postDate = new Date(posts[key][2]);
+    postDate = postDate.toLocaleString();
+    linkT.appendChild(document.createTextNode(postDate));
+
+    linkA.appendChild(linkH);
+    linkA.appendChild(linkP);
+    linkA.appendChild(linkT);
+
+    link.appendChild(linkA);
+
+    var linkD = document.createElement('div');
+    linkD.setAttribute("class", "categ");
+    var linkC = document.createElement('a');
+    linkC.setAttribute("href", "#categ" + posts[key][1][0]);
+    linkC.appendChild(document.createTextNode(categ[posts[key][1][0]]));
+
+    linkD.appendChild(linkC);
+    link.appendChild(linkD);
+
+
+    menu.appendChild(link);
+
+
+
+    var page = document.createElement('div');
+
+    page.setAttribute("class", "tab-content");
+    page.setAttribute("id", key);
+
+    var titulo = document.createElement('h1');
+    titulo.innerHTML = posts[key][0];
+    //var back = document.createElement('a');
+
+    page.appendChild(titulo);
+    page.appendChild(document.createTextNode(posts[key][6]));
+
+    folder.prepend(page);
+
 }
 
+/*
 
-function words(){  
-    document.getElementById("w").innerHTML = wordlist[i];
-    i++; 
-}
+    console.log(key);
 
-//alert("error");
+
+page.innerHTML = ;
+*/
